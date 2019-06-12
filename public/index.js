1a9fab9206740fc89f2697716eacb4009f869517
ChibaTosh <chibat@oregonstate.edu>
2019-06-11 17:10:10 -0700

    Added editing functionality

--- public/index.js
+++ public/index.js
@@ -4,35 +4,48 @@ var modalBackdrop = document.getElementById('modal-backdrop');
 var modalBackdrop2 = document.getElementById('modal-backdrop2');
 var modalButton = document.getElementById('create-goal-button');
 var modalCloseButton = document.getElementsByClassName('modal-close-button')[0];
+var modalCloseButton2 = document.getElementsByClassName('modal-close-button2')[0];
 var modalCancelButton = document.getElementsByClassName('modal-cancel-button')[0];
+var modalCancelButton2 = document.getElementsByClassName('modal-cancel-button')[0];
 var modalCreateButton = document.getElementsByClassName('modal-accept-button')[0];
+var modalCreateButton2 = document.getElementsByClassName('modal-accept-button2')[0];
 var goals = document.getElementsByClassName('goals-container');
 
 modalButton.addEventListener('click', openModal);
 modalCloseButton.addEventListener('click', closeModal);
 modalCancelButton.addEventListener('click', closeModal);
 modalCreateButton.addEventListener('click', createNewGoal);
+modalCloseButton2.addEventListener('click', closeModal2);
+modalCancelButton2.addEventListener('click', closeModal2);
+modalCreateButton2.addEventListener('click', editGoal);
+var goalSelected;
 
 for(var i = 0; i < goals.length; i++) {
 	goals[i].addEventListener('click', function(event) {
-		//console.log(event.target);
+		goalSelected = event.target;
 		selectedGoal(event.target);
 	});
 }
 
+
+function editGoal() {
+	var newGoal = document.getElementById('goal-text-input2').value;
+	var newDate = document.getElementById('goal-attribution-input2').value;
+	var newGoalBox = goalSelected.parentElement;
+	newGoalBox.getElementsByClassName("goal-text")[0].innerText = newGoal;
+	newGoalBox.getElementsByClassName("goal-date")[0].innerText = newDate;
+	closeModal2();
+}
+
 function selectedGoal(event) {
-	modalBackdrop2.style.display = 'block';
+	modalBackdrop.style.display = 'block';
 	modal2.style.display = 'block';
 	var goalText = document.getElementsByClassName("goal-text");
 	var goalTextData = event.parentElement;
 	var test = goalTextData.getElementsByClassName('goal-text')[0].innerText;
 	var test2 = goalTextData.getElementsByClassName('goal-date')[0].innerText;
-	document.getElementById('goal-text-input').value = test;
-	document.getElementById('goal-attribution-input').value = test2;
-	var createButton = document.getElementsByClassName('modal-accept-button')[0];
-	if(createButton.addEventListener) {
-		console.log('hello');
-	}
+	document.getElementById('goal-text-input2').value = test;
+	document.getElementById('goal-attribution-input2').value = test2;
 }
 
 function openModal() {
@@ -44,6 +57,13 @@ function closeModal() {
 	modalBackdrop.style.display = 'none';
 	modal.style.display = 'none';
 	document.getElementById('goal-attribution-input').value = "";
+	document.getElementById('goal-text-input').value = "";
+}
+
+function closeModal2() {
+	modalBackdrop.style.display = 'none';
+	modal2.style.display = 'none';
+	document.getElementById('goal-attribution-input2').value = "";
 	document.getElementById('goal-text-input').value = "";
 }
 
